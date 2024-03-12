a60192e02463c2ea90632f98927bf12f0a198a7c
isabelrl <isabelrl@uio.no>
2024-03-12 13:00:46 +0100

    finished homeviewmodel for mvp

--- app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/home/HomeScreenViewModel.kt
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/home/HomeScreenViewModel.kt
@@ -1,13 +1,58 @@
 package no.uio.ifi.in2000.team19.prosjekt.ui.home
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import no.uio.ifi.in2000.team19.prosjekt.data.LocationForecastRepository
+import no.uio.ifi.in2000.team19.prosjekt.data.LocationForecastRepositoryInterface
+import no.uio.ifi.in2000.team19.prosjekt.model.DTO.Advice
+import java.io.IOException
 
 // ui states
 
 // metoder for å loade data inn i ui state / oppdatere dem
 
+sealed interface AdviceUiState{
+    data class Success(val allAdvice:List<Advice>) : AdviceUiState
+    data object Loading : AdviceUiState
+    data object Error : AdviceUiState
+}
 
 class HomeScreenViewModel: ViewModel(){
+
+    private val locationForecastRepository : LocationForecastRepositoryInterface = LocationForecastRepository()
+
+    private var adviceUiState: AdviceUiState by mutableStateOf(AdviceUiState.Loading)
+
+
+    init {
+        loadAllAdvice()
+    }
+
+   private fun loadAllAdvice() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                //TODO change arguments later
+                val allAdvice = locationForecastRepository.getAdvice("10", "60")
+                adviceUiState = AdviceUiState.Success(allAdvice)
+            }
+            catch (e: IOException) {
+                AdviceUiState.Error
+            }
+            //} catch (e: HttpException) {
+            //                AlpacaUiState.Error
+            //            }
+        }
+
+    }
+
+    fun getAdviceUiState(): AdviceUiState {
+        return adviceUiState
+    }
 
     /*
     *
