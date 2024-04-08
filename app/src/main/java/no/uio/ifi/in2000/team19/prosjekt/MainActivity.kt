f16de0ba95c106e3b60cf5db90460f7ddf080a43
Markus <markusevanger@gmail.com>
2024-04-08 18:45:32 +0200

    add imports to fix merge issues.

--- app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/MainActivity.kt
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/MainActivity.kt
@@ -36,8 +36,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-
                     ScreenManager(settingsScreenViewModel, homeScreenViewModel, screenManagerViewModel)
                 }
             }
--- app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/home/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/home/HomeScreen.kt
@@ -1,7 +1,10 @@
 package no.uio.ifi.in2000.team19.prosjekt.ui.home
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +12,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -28,9 +34,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
 import com.patrykandpatrick.vico.compose.chart.CartesianChartHost
@@ -46,6 +55,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import no.uio.ifi.in2000.team19.prosjekt.data.settingsDatabase.cords.Cords
 import no.uio.ifi.in2000.team19.prosjekt.model.DTO.Advice
+import no.uio.ifi.in2000.team19.prosjekt.model.DTO.GeneralForecast
 
 
 @Composable
@@ -121,7 +131,7 @@ fun HomeScreen(advice: AdviceUiState.Success, cords: Cords) {
         Spacer(modifier = Modifier.size(50.dp))
 
         //WeatherForecast(advice.weatherForecast)
-        ForecastGraph(weatherForecast = advice.weatherForecast)
+        ForecastGraph()
     }
 }
 
@@ -165,7 +175,7 @@ fun WeatherForecast(weatherForecast: List<GeneralForecast>) {
 }
 
 @Composable
-fun ForecastGraph(weatherForecast: List<GeneralForecast>){
+fun ForecastGraph(){
 
 
     val modelProducer = remember { CartesianChartModelProducer.build() } // burde flyttes til viewmodel https://patrykandpatrick.com/vico/wiki/cartesian-charts/data (se første warning)
