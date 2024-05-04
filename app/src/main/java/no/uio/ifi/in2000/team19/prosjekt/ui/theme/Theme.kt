de77f6da9fc5152d1f8b6a64f0c46e173a4ff7ec
Markus <markusevanger@gmail.com>
2024-05-04 17:17:06 +0200

    HomeScreen + Add info buttons for showing info + Customize graph more.
    Still cant get shader to work properly.
    
    + Add material design to SearchLocation
    
    + Remove top bar and make it transparent :D

--- app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/theme/Theme.kt
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package no.uio.ifi.in2000.team19.prosjekt.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,12 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 
 private val DarkColorPalette = darkColorScheme(
@@ -125,17 +119,6 @@ fun Team19prosjektoppgaveTheme(
         darkTheme -> DarkColorPalette
         else -> LightColorPalette
 
-    }
-
-    val TOP_APP_BAR_COLOR = Color.Blue.toArgb() // This is the top app bar color. Needs to be the same color as the top color of vertical gradient in HomeScreen
-
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = TOP_APP_BAR_COLOR
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
     }
 
     MaterialTheme(
