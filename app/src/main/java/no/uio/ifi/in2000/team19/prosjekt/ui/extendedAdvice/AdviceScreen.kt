70747a3f9f1fdbf4c3a849c73a66e90b08b46308
synnobi <synnobi@uio.no>
2024-05-06 01:20:52 +0200

    start fixing advice screen

--- app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/extendedAdvice/AdviceScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/extendedAdvice/AdviceScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -21,6 +23,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -62,25 +65,61 @@ fun AdviceScreen(adviceId: Int, navController: NavController, viewModel: HomeScr
     ) {innerpadding->
         Box(modifier = Modifier.padding(innerpadding)) {
 
-            Column(
+            Text(advice.title, fontWeight = FontWeight.Bold, modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp, horizontal = 10.dp))
+
+
+            LazyColumn(
                 modifier = Modifier
                     .padding(30.dp)
                     .fillMaxWidth()
                     .fillMaxHeight()
-                    .verticalScroll(rememberScrollState())
-            ) {
-                Text(advice.title, fontWeight = FontWeight.Bold, modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp, horizontal = 10.dp))
+                    .verticalScroll(rememberScrollState())) {
 
-                Text(advice.description)
+                val description: String = advice.description
 
+                val paragraphs = description.split("\n")
+
+
+                items(paragraphs) { paragraph ->
+                    if (paragraph[0].toString() == "~") {
+                        Text(text = paragraph.substring(1), style = MaterialTheme.typography.headlineSmall)
+                    }
+                    else {
+                        Text(text = paragraph)
+                    }
+                }
             }
+
+
         }
 
     }
 
-
-
 }
 
+/*
+@Composable
+fun SingleStringLayout(stringFromXml: String) {
+    val paragraphs = stringFromXml.split("\n")
+
+        LazyColumn {
+            items(paragraphs) { paragraph ->
+                if (paragraph[0].toString() == "~") {
+                    Text(text = paragraph.substring(1), style = MaterialTheme.typography.headlineSmall)
+                }
+                else {
+                    Text(text = paragraph)
+                }
+            }
+        }
+}
+
+
+                    if (it[0].toString() == "~"){
+                        Text(it.substring(1), style = MaterialTheme.typography.bodyLarge)
+                    }
+                    else {
+
+ */
