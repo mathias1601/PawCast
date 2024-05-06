790c932ade4b0b93f457684f6bf951877af8a851
synnobi <synnobi@uio.no>
2024-05-07 00:19:34 +0200

    find out formatting issue, start formatting strings to advice screen

--- app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/extendedAdvice/AdviceScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/extendedAdvice/AdviceScreen.kt
@@ -2,15 +2,19 @@ package no.uio.ifi.in2000.team19.prosjekt.ui.extendedAdvice
 
 
 import android.os.Build
+import android.widget.Space
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
@@ -21,11 +25,15 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import no.uio.ifi.in2000.team19.prosjekt.model.DTO.Advice
@@ -39,6 +47,7 @@ fun AdviceScreen(adviceId: Int, navController: NavController, viewModel: HomeScr
     val advice: Advice = viewModel.collectAdviceById(adviceId)
 
     Scaffold (
+
         topBar = {
             CenterAlignedTopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -63,32 +72,52 @@ fun AdviceScreen(adviceId: Int, navController: NavController, viewModel: HomeScr
             )
         }
     ) {innerpadding->
+
         Box(modifier = Modifier.padding(innerpadding)) {
-
-            Text(advice.title, fontWeight = FontWeight.Bold, modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 10.dp, horizontal = 10.dp))
-
 
             LazyColumn(
                 modifier = Modifier
                     .padding(30.dp)
                     .fillMaxWidth()
-                    .fillMaxHeight()
-                    .verticalScroll(rememberScrollState())) {
+                    .fillMaxHeight()) {
 
-                val description: String = advice.description
+                item {
+                    Text(advice.title, modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .fillMaxHeight()
+                        .padding(vertical = 10.dp, horizontal = 10.dp)
+                        .background(
+                            color = Color(MaterialTheme.colorScheme.primary.hashCode()),
+                            shape = RoundedCornerShape(
+                                topEnd = 23.dp,
+                                topStart = 23.dp
+                            )
+                        ),
+                        color = Color.White
+                    )
+
+                    Spacer(modifier = Modifier.padding(20.dp))
+                }
+
+
+                var description: String = advice.description
 
                 val paragraphs = description.split("\n")
 
 
                 items(paragraphs) { paragraph ->
-                    if (paragraph[0].toString() == "~") {
-                        Text(text = paragraph.substring(1), style = MaterialTheme.typography.headlineSmall)
+                    val trimmedParagraph = paragraph.trim()
+                    if (trimmedParagraph.length > 0) {
+                        if (trimmedParagraph.startsWith("~")) {
+                            Spacer(modifier = Modifier.padding(5.dp))
+                            Text(text = trimmedParagraph.substring(1), style = MaterialTheme.typography.headlineSmall)
+                            Spacer(modifier = Modifier.padding(5.dp))
+                        }
+                        else {
+                            Text(text = trimmedParagraph)
+                        }
                     }
-                    else {
-                        Text(text = paragraph)
-                    }
+
                 }
             }
 
