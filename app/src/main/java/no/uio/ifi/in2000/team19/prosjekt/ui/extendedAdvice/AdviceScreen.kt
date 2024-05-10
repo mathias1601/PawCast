bb3b57a08115e60e1430e5bf27dfb5c80f77e079
Markus <markusevanger@gmail.com>
2024-05-10 16:30:31 +0200

    fix merge errors.

--- app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/extendedAdvice/AdviceScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/ui/extendedAdvice/AdviceScreen.kt
@@ -1,17 +1,6 @@
 package no.uio.ifi.in2000.team19.prosjekt.ui.extendedAdvice
 
 
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ChevronLeft
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -20,16 +9,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import no.uio.ifi.in2000.team19.prosjekt.R
-import no.uio.ifi.in2000.team19.prosjekt.model.DTO.Advice
-import no.uio.ifi.in2000.team19.prosjekt.ui.home.HomeScreenViewModel
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
