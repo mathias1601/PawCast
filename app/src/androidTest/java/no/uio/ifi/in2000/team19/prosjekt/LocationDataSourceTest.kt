809d8a32a272ce70128338bbdf04cc36c1c77445
Markus <markusevanger@gmail.com>
2024-05-15 16:44:44 +0200

    write LocationForecastDataSource instrumented tests
    + change LocationForecastDataSource to use a try catch and return null if an error happens.

--- app/src/androidTest/java/no/uio/ifi/in2000/team19/prosjekt/LocationDataSourceTest.kt
+++ app/src/androidTest/java/no/uio/ifi/in2000/team19/prosjekt/LocationDataSourceTest.kt
@@ -4,22 +4,26 @@ import kotlinx.coroutines.runBlocking
 import no.uio.ifi.in2000.team19.prosjekt.data.LocationForecastDataSource
 import no.uio.ifi.in2000.team19.prosjekt.model.dto.locationForecast.LocationForecast
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 
+/**
+ * Mostly just tests that the API actually works, but also
+ * tests that behaviour is as expected.
+ *
+ * A better test should mock clients and more: https://ktor.io/docs/client-testing.html#add_dependencies
+ * Making actual network tests are not recomended, and as technical debt we should ideally create a Unit Test
+ * and change dataSource to accept the KTOR http client as a parameter, so we can mock it in the unit test.
+ * + technical debt on writing better tests for datasource.
+ *
+ * Needs to be an instrumented test to actually make network calls
+ *
+ * But its also nice to test the ifi proxy / MET api in our opinion.*/
 class LocationDataSourceTest {
 
 
 
     @Test
-    /** Mostly just tests that the API actually works, but also
-     * tests that behaviour is as expected.
-     *
-     * A better test should mock clients and more: https://ktor.io/docs/client-testing.html#add_dependencies
-     * + technical debt on writing better tests for datasource.
-     *
-     * But its also nice to test the ifi proxy / MET api in our opinion.
-     * */
     fun get_valid_result_with_valid_coordinates(){
 
         val dataSource = LocationForecastDataSource()
@@ -35,7 +39,7 @@ class LocationDataSourceTest {
             apiResult = dataSource.getLocationForecast(latitude, longitude)
         }
         // ASSERT
-        assertNotEquals(null, apiResult)
+        assertNotNull(apiResult)
 
     }
 
