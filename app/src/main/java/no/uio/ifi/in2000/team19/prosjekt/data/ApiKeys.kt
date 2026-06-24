6f96a4ec2570ee363dc7122b57f82b82c2232f0e
mathias1601 <mathias.xinyu.yang@gmail.com>
2026-06-24 15:26:09 +0200

    init commit

---
+++ app/src/main/java/no/uio/ifi/in2000/team19/prosjekt/data/ApiKeys.kt
@@ -0,0 +1,12 @@
+package no.uio.ifi.in2000.team19.prosjekt.data
+
+
+/** Fill in your own keys here */
+object ApiKeys {
+    const val USE_PROXY = false // only use if you have access to the IFI proxy.
+    const val PROXY_KEY = "" // ifi proxy here
+
+    // get here: https://docs.mapbox.com/android/search/guides/get-started/
+    const val MAPBOX_PUBLIC_ACCESS_TOKEN = "replace this"
+    // Navigate to gradle.properties to put in your secret access token.
+}
